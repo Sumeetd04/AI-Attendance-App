c8b4895744791e4ddce12e8cb290cf692473f7e7
Sumeetd04 <sumeetdhangare38@gmail.com>
2026-09-24 22:41:22 +0530

    Add project README

---
+++ src/components/footer.py
@@ -0,0 +1,6 @@
+def footer_home():
+    pass
+
+
+def footer_dashboard():
+    pass
---
+++ src/ui/base_layout.py
@@ -0,0 +1,11 @@
+from src.ui.teacher_screen import (
+    style_background_dashboard,
+    style_background_home,
+    style_base_layout,
+)
+
+__all__ = [
+    "style_background_dashboard",
+    "style_background_home",
+    "style_base_layout",
+]
